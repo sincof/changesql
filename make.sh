a5bb591b07e85100ba3915e9bfee6211b84806ad
haesaki <84838062+Haesaki@users.noreply.github.com>
2021-12-19 15:35:53 +0800

    add shell

---
+++ make.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p target
+javac -sourcepath ./src/ ./src/com/tencent/Example.java  -d ./target/
---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# ./start.sh  --data_path /tmp/data --dst_ip 127.0.0.1 --dst_port 3306 --dst_user root --dst_password 123456789
+
+cd target
+
+echo "shell parameters: $*"
+
+java -classpath . com.tencent.Example  $*
