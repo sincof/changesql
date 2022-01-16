e414bfadb147a49545d95a9584ecf42516cd96d6
hasaeki <84838062+Haesaki@users.noreply.github.com>
2022-01-16 23:45:50 +0800

    fix statement error

--- src/com/sin/thread/InsertByDatabase.java
+++ src/com/sin/thread/InsertByDatabase.java
@@ -6,7 +6,9 @@ import com.sin.service.DBConnection;
 import net.sf.jsqlparser.statement.create.table.Index;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -48,25 +50,27 @@ public class InsertByDatabase implements Callable<Boolean> {
                 // 用于主键判断的条件
                 StringBuilder primaryStat = new StringBuilder();
                 Map<String, Integer> primaryKeyIndex = new HashMap<>();
-                for (Index index : list) {
-                    if ("primary key".equals(index.getType().toLowerCase(Locale.ROOT))) {
-                        primaryorUniqueKey = true;
-                        primaryKey = index.getColumnsNames();
-                        for (String s : primaryKey) {
-                            if (primaryStat.length() == 0)
-                                primaryStat = new StringBuilder(s + "=?");
-                            else
-                                primaryStat.append(" and ").append(s).append("=?");
-                            for (int i = 0; i < curTable.columns.size(); i++) {
-                                if (curTable.columns.get(i).equals(s)) {
-                                    primaryKeyIndex.put(s, i);
+                if(list != null){
+                    for (Index index : list) {
+                        if ("primary key".equals(index.getType().toLowerCase(Locale.ROOT))) {
+                            primaryorUniqueKey = true;
+                            primaryKey = index.getColumnsNames();
+                            for (String s : primaryKey) {
+                                if (primaryStat.length() == 0)
+                                    primaryStat = new StringBuilder(s + "=?");
+                                else
+                                    primaryStat.append(" and ").append(s).append("=?");
+                                for (int i = 0; i < curTable.columns.size(); i++) {
+                                    if (curTable.columns.get(i).equals(s)) {
+                                        primaryKeyIndex.put(s, i);
+                                    }
                                 }
                             }
+                            break;
+                        } else if ("unique key".equals(index.getType().toLowerCase(Locale.ROOT))) {
+                            primaryorUniqueKey = true;
+                            break;
                         }
-                        break;
-                    } else if ("unique key".equals(index.getType().toLowerCase(Locale.ROOT))) {
-                        primaryorUniqueKey = true;
-                        break;
                     }
                 }
                 if (primaryorUniqueKey) {
@@ -102,50 +106,52 @@ public class InsertByDatabase implements Callable<Boolean> {
                         String line = br.readLine();
                         String[] data;
                         while (line != null) {
-                            data = line.split(",");
-                            for (int i = 0; i < data.length; i++) {
-                                insertStatement.setString(i + 1, data[i]);
-                                updateStatement.setString(i + 1, data[i]);
-                            }
-                            // 设置where的主键等于语句
-                            int nowKeyIndex = 1;
-                            for (String pk : primaryKey) {
-                                updateStatement.setString(data.length + nowKeyIndex, data[primaryKeyIndex.getOrDefault(pk, 0)]);
-                                selectStatement.setString(nowKeyIndex, data[primaryKeyIndex.getOrDefault(pk, 0)]);
-                                nowKeyIndex++;
-                            }
-                            ResultSet resultSet = selectStatement.executeQuery();
-                            if (resultSet.next()) {
-                                // 结果集中存在数据 更新数据
-                                String updated_at = resultSet.getString(1);
-                                try {
-                                    // < 0 is updated_at is before the data[updatedatIndex]
-                                    // > 0 is updated_at is after the data[updatedatIndex]
-                                    if (compareTime(updated_at, data[updatedatIndex]) < 0) {
-                                        updateStatement.addBatch();
-                                        updateCnt++;
-                                    }
-                                } catch (ParseException pe) {
-                                    System.out.println("updated_at in DB: " + updated_at + ", updated_at in data: " + data[updatedatIndex]);
-                                    pe.printStackTrace();
+                            if (line.length() != 0) {
+                                data = line.split(",");
+                                for (int i = 0; i < data.length; i++) {
+                                    insertStatement.setString(i + 1, data[i]);
+                                    updateStatement.setString(i + 1, data[i]);
                                 }
-                                // 判断是否更新的函数还没写，默认更新
-                                // TODO: ERROR Duplicate entry '1784783537-595.527' for key '1.PRIMARY'
-                                // 1. 数据库中存在了和插入数据一样的主键，但是没有查询出来
-                                // 2. 盲猜就是数据精度出现了问题，虽然没有查询到主键，但是实际上插入的时候却会报这个错误
-                                // updateStatement.execute();
-                            } else {
-                                // 没有结果 就插入数据
-                                // insertStatement.execute();
-                                insertStatement.addBatch();
-                                insertCnt++;
+                                // 设置where的主键等于语句
+                                int nowKeyIndex = 1;
+                                for (String pk : primaryKey) {
+                                    updateStatement.setString(data.length + nowKeyIndex, data[primaryKeyIndex.getOrDefault(pk, 0)]);
+                                    selectStatement.setString(nowKeyIndex, data[primaryKeyIndex.getOrDefault(pk, 0)]);
+                                    nowKeyIndex++;
+                                }
+                                ResultSet resultSet = selectStatement.executeQuery();
+                                if (resultSet.next()) {
+                                    // 结果集中存在数据 更新数据
+                                    String updated_at = resultSet.getString(1);
+                                    try {
+                                        // < 0 is updated_at is before the data[updatedatIndex]
+                                        // > 0 is updated_at is after the data[updatedatIndex]
+                                        if (compareTime(updated_at, data[updatedatIndex]) < 0) {
+                                            updateStatement.addBatch();
+                                            updateCnt++;
+                                        }
+                                    } catch (ParseException pe) {
+                                        System.out.println("updated_at in DB: " + updated_at + ", updated_at in data: " + data[updatedatIndex]);
+                                        pe.printStackTrace();
+                                    }
+                                    // 判断是否更新的函数还没写，默认更新
+                                    // TODO: ERROR Duplicate entry '1784783537-595.527' for key '1.PRIMARY'
+                                    // 1. 数据库中存在了和插入数据一样的主键，但是没有查询出来
+                                    // 2. 盲猜就是数据精度出现了问题，虽然没有查询到主键，但是实际上插入的时候却会报这个错误
+                                    // updateStatement.execute();
+                                } else {
+                                    // 没有结果 就插入数据
+                                    // insertStatement.execute();
+                                    insertStatement.addBatch();
+                                    insertCnt++;
+                                }
+                                resultSet.close();
                             }
-                            resultSet.close();
                             line = br.readLine();
-                            if (updateCnt >= 100000) {
+                            if (updateCnt >= 10000) {
                                 updateStatement.executeBatch();
                             }
-                            if (insertCnt >= 100000) {
+                            if (insertCnt >= 10000) {
                                 insertStatement.executeBatch();
                             }
                         }
@@ -166,26 +172,31 @@ public class InsertByDatabase implements Callable<Boolean> {
                     }
                 } else {
                     // 在tableEntity中，createTable只在第一次的时候更新过，后面的更新都记录在Map里面
-                    StringBuilder selectSB = new StringBuilder("select updated_at from " + tableName + "where ");
+                    StringBuilder selectSB = new StringBuilder("select updated_at from " + tableName + " where ");
+                    StringBuilder updateSB = new StringBuilder("update " + tableName + " set updated_at = ? where ");
                     StringBuilder insertSB = new StringBuilder("insert into " + tableName + " values (");
                     int updatedatIndex = 0, cnt = 0;
                     for (String name : curTable.columns) {
                         insertSB.append("?,");
                         if (name.toLowerCase(Locale.ROOT).contains("updated_at")) {
                             updatedatIndex = cnt;
-                        } else
-                            selectSB.append(name).append("=?,");
+                        } else {
+                            selectSB.append(name).append("=? and");
+                            updateSB.append(name).append("=?,");
+                        }
                         cnt++;
                     }
                     // 把insertSB的最后一个逗号删除掉，然后添加)
                     insertSB.deleteCharAt(insertSB.length() - 1);
                     insertSB.append(")");
-                    // 把insertSB的最后一个逗号删除掉，然后添加)
-                    selectSB.deleteCharAt(selectSB.length() - 1);
-                    selectSB.append(")");
+                    // 把selectSB的最后一个逗号删除掉 delete final `and`
+                    // selectSB.deleteCharAt(selectSB.length() - 1);
+                    selectSB.delete(selectSB.length() - 3, selectSB.length());
+                    // 把updateSB的最后一个逗号删除掉
+                    updateSB.deleteCharAt(updateSB.length() - 1);
 
                     PreparedStatement selectStatement = connection.prepareStatement(selectSB.toString());
-                    PreparedStatement updateStatement = connection.prepareStatement("update " + tableName + " set updated_at = ?");
+                    PreparedStatement updateStatement = connection.prepareStatement(updateSB.toString());
                     PreparedStatement insertStatement = connection.prepareStatement(insertSB.toString());
 
                     // 对所有插入的语句进行一个个计数
@@ -195,40 +206,47 @@ public class InsertByDatabase implements Callable<Boolean> {
                         String line = br.readLine();
                         String[] data;
                         while (line != null) {
-                            data = line.split(",");
-                            for (int i = 0; i < data.length; i++) {
-                                insertStatement.setString(i + 1, data[i]);
-                                if (i < updatedatIndex)
-                                    selectStatement.setString(i + 1, data[i]);
-                                else if (i == updatedatIndex)
-                                    updateStatement.setString(1, data[i]);
-                                else selectStatement.setString(i, data[i]);
-                            }
-                            ResultSet resultSet = selectStatement.executeQuery();
-                            if (resultSet.next()) {
-                                updateStatement.addBatch();
-                                updateCnt++;
-                                // updateStatement.execute();
-                            } else {
-                                insertStatement.addBatch();
-                                insertCnt++;
-                                //insertStatement.execute();
+                            if (line.length() != 0) {
+                                data = line.split(",");
+                                for (int i = 0; i < data.length; i++) {
+                                    insertStatement.setString(i + 1, data[i]);
+                                    if (i < updatedatIndex) {
+                                        selectStatement.setString(i + 1, data[i]);
+                                        updateStatement.setString(i + 2, data[i]);
+                                    }
+                                    else if (i == updatedatIndex)
+                                        updateStatement.setString(1, data[i]);
+                                    else {
+                                        selectStatement.setString(i, data[i]);
+                                        updateStatement.setString(i + 2, data[i]);
+                                    }
+                                }
+                                ResultSet resultSet = selectStatement.executeQuery();
+                                if (resultSet.next()) {
+                                    updateStatement.addBatch();
+                                    updateCnt++;
+                                    // updateStatement.execute();
+                                } else {
+                                    insertStatement.addBatch();
+                                    insertCnt++;
+                                    //insertStatement.execute();
+                                }
                             }
                             line = br.readLine();
-                            if (updateCnt >= 100000) {
+                            if (updateCnt >= 10000) {
                                 updateStatement.executeBatch();
                             }
-                            if (insertCnt >= 100000) {
+                            if (insertCnt >= 10000) {
                                 insertStatement.executeBatch();
                             }
                         }
-                        br.close();
                         if (updateCnt != 0) {
                             updateStatement.executeBatch();
                         }
                         if (insertCnt != 0) {
                             insertStatement.executeBatch();
                         }
+                        br.close();
                     }
                     try {
                         insertStatement.close();
@@ -236,15 +254,17 @@ public class InsertByDatabase implements Callable<Boolean> {
                         updateStatement.close();
                     } catch (SQLException e) {
                         e.printStackTrace();
+                        return false;
                     }
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (SQLException | IOException ex) {
+            ex.printStackTrace();
             return false;
         }
         return true;
     }
+
 
     // a < b: return true
     // a > b: return false
